07cbb686754435d9e0e7a02ee76af58735c5d0b7
Kourtney Burger <kourtney.burger@noaa.gov>
2023-09-08 10:40:40 -0700

    updated passive packer script

--- PassivePacker_Deployment.R
+++ PassivePacker_Deployment.R
@@ -14,8 +14,8 @@ DepDir <- here('ADRIFT-Deployment-PassivePacker')
 deployDetails <- read.csv("DeploymentDetails/CCES_DeploymentDetails.csv")
 
 #Import PassivePacker sqlite Data Table
-db <- dbConnect(SQLite(), "PassivePacker_v.4.0.2-win64/database/packageData.sqlite")
-PP_DeploymentData <- dbReadTable(db, 'DEPLOYMENT_DATA')
+#db <- dbConnect(SQLite(), "PassivePacker_v.4.0.2-win64/database/packageData.sqlite")
+#PP_DeploymentData <- dbReadTable(db, 'DEPLOYMENT_DATA')
 
 
 #Set up Passive Packer data table
@@ -50,6 +50,12 @@ colnames(dd) <- c('ID','PROJECT','DEPLOYMENT_ID','SITE_CRUISE','SITE_OR_CRUISE',
                   'SCIENTISTS','SPONSORS','FUNDERS','SENSORS','DEPLOYMENT_DETAILS',
                   'CALIBRATION_INFO','DATASET_INFO','ANCILLARY_INFO')   
 
+dd$PROJECT <- paste('SWFSC_',dd$PROJECT)
+dd$PROJECT <- gsub(" ","",dd$PROJECT)
+
+dd$PACKAGE_ID <- paste(dd$PROJECT,'_',dd$SITE_CRUISE,'_',dd$DEPLOYMENT_ID)
+dd$PACKAGE_ID <- gsub(" ","",dd$PACKAGE_ID)
+
   #Change soundtrap names
 dd[dd == "ST640"] <- "SoundTrap 640"
 dd[dd == "ST4300HF"] <- "SoundTrap 4300 HF"
@@ -59,84 +65,16 @@ dd[dd == "ST500HF"] <- "SoundTrap 500 HF"
 dd[dd == "ST300HF"] <- "SoundTrap 300 HF"
 
 #The following info will stay the same for each project (CCES, PASCAL, ADRIFT)
-dd <- 
+dd["TITLE"][is.na(dd["TITLE"])] <- 'California Current Ecosystem Survey'
+dd["PURPOSE"][is.na(dd["PURPOSE"])] <- 'From Report'
+dd["ABSTRACT"][is.na(dd["ABSTRACT"])] <- 'From Report'
 
-#Loop
 
-#required packages
-library(dplyr)
-library(xlsx)
-library(openxlsx)
-library(stringr)
-library(here)
-library(lubridate)
+#Write dataframe to existing database
+con <- dbConnect(SQLite(), "PassivePacker_v.4.0.2-win64/database/packageData.sqlite")
+dbWriteTable(con, 'DEPLOYMENT_DATA', dd, overwrite = TRUE)
+dbDisconnect(con)
 
-makeTethysDep<-function(ProjectID = 'CCES',DepID = '004'){
-  
-  DepDir<-here('Deployment Worksheets')
-  
-  #load deployment info
-  setwd(DepDir)
-  AllDeployments<-xlsx::read.xlsx('Deployment Details.xlsx',sheetName='deployDetails')
-  
-  #Correct Date/Time formats
-  #Dates and times may be read in as characters if NAs are present
-  if(is.character(AllDeployments$Deployment_Date)){
-    AllDeployments$Recovery_Date<-openxlsx::convertToDateTime(AllDeployments$Deployment_Date)}
-  if(is.character(AllDeployments$Recovery_Date)){
-    AllDeployments$Recovery_Date<-openxlsx::convertToDateTime(AllDeployments$Recovery_Date)}
-  if(is.character(AllDeployments$Data_Start)){
-    AllDeployments$Data_Start<-openxlsx::convertToDateTime(AllDeployments$Data_Start)}
-  if(is.character(AllDeployments$Data_End)){
-    AllDeployments$Data_End<-openxlsx::convertToDateTime(AllDeployments$Data_End)}
-  #All dates and datetimes must be saved as POSIXct for Tethys to understand them
-  AllDeployments$Deployment_Date<-as.POSIXct(AllDeployments$Deployment_Date,'%m/%d/%Y',tz='UTC')
-  AllDeployments$Recovery_Date<-as.POSIXct(AllDeployments$Recovery_Date,'%m/%d/%Y',tz='UTC')
-  AllDeployments$Data_Start<-as.POSIXct(AllDeployments$Data_Start,"%m/%d/%Y %H:%M",tz='UTC')
-  AllDeployments$Data_End<-as.POSIXct(AllDeployments$Data_End,"%m/%d/%Y %H:%M",tz='UTC')
-  
-  AllDeployments<-AllDeployments %>%
-    mutate(Depth_Sensor=as.numeric(Depth_Sensor),
-           Recovery_Latitude=as.numeric(Recovery_Latitude),
-           Recovery_Longitude=as.numeric(Recovery_Longitude),
-           SensorNumber_1=as.numeric(SensorNumber_1),
-           SensorNumber_2=as.numeric(SensorNumber_2),
-           SensorNumber_3=as.numeric(SensorNumber_3))
-  
-  #create dataframe for individual deployment 
-  Deployment<-AllDeployments %>% 
-    filter(Project==ProjectID,DeploymentID==as.numeric(DepID)) 
-  
-  #create separate dataframe for sensors
-  Sensors<-Deployment %>%
-    summarize(Data_ID=Data_ID,
-              ChannelNumber=c(ChannelNumber_1,ChannelNumber_2,ChannelNumber_3),
-              SensorNumber=c(SensorNumber_1,SensorNumber_2,SensorNumber_3)) %>%
-    mutate(ChannelNumber=as.numeric(ChannelNumber)) %>%
-    na.omit()
-  
-  #remove sensor info from deployment dataframe
-  Deployment<-Deployment %>%
-    select(-c(ChannelNumber_1,ChannelNumber_2,ChannelNumber_3,
-              SensorNumber_1,SensorNumber_2,SensorNumber_3))
-  
-  #load GPS data
-  GPSDir<-here('Deployment Worksheets','GPS')
-  GPSfiles<-dir(path=GPSDir,pattern='GPS')
-  GPSInd<-which(str_match(GPSfiles,paste0(ProjectID,'_',DepID))>0)
-  GPSdata<-read.csv(file.path(GPSDir,GPSfiles[GPSInd]))
-  GPSdata$UTC<-as.POSIXct(GPSdata$UTC,format='%Y-%m-%d %H:%M:%S',tz='UTC')
-  
-  #Create worksheet
-  DepFile<-paste0(ProjectID,'_',DepID,'_DeployDetails.xlsx')
-  DepFilePath = file.path(DepDir,DepFile)
-  
-  wb = createWorkbook()
-  shtDeployDetails = addWorksheet(wb, "deployDetails")
-  shtGPS = addWorksheet(wb, "GPS")
-  shtSensor = addWorksheet(wb, "Sensor")
-  writeData(wb,shtDeployDetails,Deployment)
-  writeData(wb,shtGPS,GPSdata) 
-  writeData(wb,shtSensor,Sensors) 
-  saveWorkbook(wb,DepFilePath,overwrite=TRUE)
-}
+db <- dbConnect(SQLite(), "PassivePacker_v.4.0.2-win64/database/packageData.sqlite")
+PP_DeploymentData <- dbReadTable(db, 'DEPLOYMENT_DATA')
+
